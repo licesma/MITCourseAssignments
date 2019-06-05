4babaa3787dc8298da1cbc08a312f5b71b940d78
licesma <36255528+licesma@users.noreply.github.com>
2019-06-05 13:58:15 -0500

    Added the 2nd problem of assignment 1

---
+++ Assignment1/2.1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char* print;
+    print= "Hello, World!";
+    cout<< print ;
+    return 0;
+}
---
+++ Assignment1/2.2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    if (n>0) {
+        int i;
+        for (i=0;i<n;i++){
+            cout<<"Hello, World\n";
+        }
+    }
+    return 0;
+}
